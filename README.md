1871d52ed1a74ea119a0c801cd2a6b2ef4f5830e
Chen <chenjiaxu333@gmail.com>
2021-07-16 04:15:19 +0900

    finished

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 ## Snake
 ### tech stack
 - TypeScript
+    - ypescript
+    - ts-loader
+
 - Less
+    - less
+    - less-loader
+    - style-loader
+    - css-loader
+
 - Webpack
+    - webpack-cli
+    - webpack-dev-server
+    - html-webpack-plugin
+    - clean-webpack-plugin
+
 ---
 ![](./src/images/01.png)
+
+---
+
+![](./src/images/02.png)
+
+---
 
 ### Control
 - w :  up
--- src/class/GameControl.ts
+++ src/class/GameControl.ts
@@ -1,104 +1,119 @@
+// 引入其他的类
 import Snake from "./Snake";
 import Food from "./Food";
-import ScoreBar from "./Bar";
+import ScorePanel from "./ScorePanel";
 
-// control all Class
-export default class GameControl {
+// 游戏控制器，控制其他的所有类
+class GameControl {
+    // 蛇
     snake: Snake;
+    // 食物
     food: Food;
-    ScoreBar: ScoreBar;
-    // default snake go down
-    direction: string = 's';
-    // gameover
-    isOver: boolean = false;
+    // 记分牌
+    scorePanel: ScorePanel;
+    // 创建一个属性来存储蛇的移动方向（也就是按键的方向）
+    direction: string = '';
+    // 创建一个属性用来记录游戏是否结束
+    isLive = true;
 
     constructor() {
         this.snake = new Snake();
         this.food = new Food();
-        this.ScoreBar = new ScoreBar();
+        this.scorePanel = new ScorePanel(10, 1);
 
-        this.init()
+        this.init();
     }
 
-    // init statrt game
+    // 游戏的初始化方法，调用后游戏即开始
     init() {
-        // snake move
-        this.move();
-        // keyup event
-        document.addEventListener('keydown', this.keyDownHandler.bind(this));
+        // 绑定键盘按键按下的事件
+        document.addEventListener('keydown', this.keydownHandler.bind(this));
+        // 调用run方法，使蛇移动
+        this.run();
     }
 
-    // keyup event
-    keyDownHandler(event: KeyboardEvent) {
+    /*
+        Chrome       IE
+        ArrowUp      Up
+        ArrowDown    Down
+        ArrowLeft    Left
+        ArrowRight   Right
+    */
+
+    // 创建一个键盘按下的响应函数
+    keydownHandler(event: KeyboardEvent) {
+        // 需要检查event.key的值是否合法（用户是否按了正确的按键）
+        // 修改direction属性
         this.direction = event.key;
-        // console.log(this.direction);
     }
 
+    // 创建一个控制蛇移动的方法
+    run() {
+        /*
+        *   根据方向（this.direction）来使蛇的位置改变
+        *       向上 top  减少
+        *       向下 top  增加
+        *       向左 left 减少
+        *       向右 left 增加
+        */
+        // 获取蛇现在坐标
+        let X = this.snake.X;
+        let Y = this.snake.Y;
 
-    // snake move
-    move() {
-        // according to this.direction's value
-        let x = this.snake.x;
-        let y = this.snake.y;
-        console.log(x, y);
-
-
-        // chage x and y
-        // a s d w
+        // 根据按键方向来计算X值和Y值（未更新）
         switch (this.direction) {
-            case "a":
-            case "A":
-                x -= 10;
+            case "w":
+            case "W":
+                // 向上移动 top 减少
+                Y -= 10;
                 break;
             case "s":
             case "S":
-                y += 10;
+                // 向下移动 top 增加
+                Y += 10;
+                break;
+            case "a":
+            case "A":
+                // 向左移动 left 减少
+                X -= 10;
                 break;
             case "d":
             case "D":
-                x += 10;
+                // 向右移动 left 增加
+                X += 10;
                 break;
-            case "w":
-            case "W":
-                y -= 10;
-                break;
-        };
+        }
 
-        //
+        // 检查蛇是否吃到了食物
+        this.checkEat(X, Y);
+
+        //修改蛇的X和Y值
         try {
-            // change snake's position
-            this.snake.x = x;
-            this.snake.y = y;
-        } catch (err) {
-            alert(err.message);
-            this.isOver = true;
+            this.snake.X = X;
+            this.snake.Y = Y;
+        } catch (e) {
+            // 进入到catch，说明出现了异常，游戏结束，弹出一个提示信息
+            alert(e.message + ' GAME OVER!');
+            // 将isLive设置为false
+            this.isLive = false;
         }
 
-
-        // check snake eat food or not
-        this.checkEat(x, y)
-
-
-        // 
-        if (!this.isOver) {
-            setTimeout(this.move.bind(this), 500 - (this.ScoreBar.level - 1) * 100)
-        }
+        // 开启一个定时调用（定时器调用自身）
+        // 会再次创建一个定时器
+        this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
     }
 
-    // check snake eat food or not
-    checkEat(x: number, y: number) {
-        if (x === this.food.x && y === this.food.y) {
-            console.log('ate');
-            // 1. change food postion
+    // 定义一个方法，用来检查蛇是否吃到食物
+    checkEat(X: number, Y: number) {
+        if (X === this.food.X && Y === this.food.Y) {
+            // 食物的位置要进行重置
             this.food.change();
-            // 2. add score
-            this.ScoreBar.addScore()
-            // 3. add snake body
-            this.snake.addBody()
-
-
+            // 分数增加
+            this.scorePanel.addScore();
+            // 蛇要增加一节
+            this.snake.addBody();
         }
-
     }
 }
 
+export default GameControl;
