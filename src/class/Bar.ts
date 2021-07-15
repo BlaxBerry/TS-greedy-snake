03f5064152ff2d04711419d855bb07bb8ad95671
Chen <chenjiaxu333@gmail.com>
2021-07-16 01:37:29 +0900

    ScoreBar class

---
+++ src/class/Bar.ts
@@ -0,0 +1,33 @@
+// message bar class
+export default class ScoreBar {
+    score: number = 0;
+    level: number = 1;
+    scoreElement: HTMLElement;
+    levelElement: HTMLElement;
+    maxLevel: number;
+    upScore: number;
+
+    constructor(maxLevel: number = 10, upScore: number = 10) {
+        this.scoreElement = document.getElementById('score')!;
+        this.levelElement = document.getElementById('level')!;
+        this.maxLevel = maxLevel;
+        this.upScore = upScore;
+    }
+
+    // score add
+    addScore() {
+        this.score++;
+        this.scoreElement.innerHTML = this.score + '';
+        if (this.score % this.upScore === 0) {
+            this.upLevel()
+        }
+    }
+
+    // level up
+    upLevel() {
+        if (this.level < this.maxLevel) {
+            this.level++;
+            this.levelElement.innerHTML = this.level + '';
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,21 @@
 // import less
 import './style/index.less'
 
-// food class
-class Food {
-    // food element
-    element: HTMLElement;
-
-    constructor() {
-        // get the element in html page
-        this.element = document.getElementById('food')!;
-    }
-
-    // get position X
-    get x() {
-        return this.element.offsetLeft;
-    }
-    // get position Y
-    get y() {
-        return this.element.offsetTop;
-    }
-
-    // change position
-    change() {
-        // (0 ~ stage.width-this.food.width) (0~290)
-        let left = Math.round(Math.random() * 29) * 10;
-        let top = Math.round(Math.random() * 29) * 10;
-        this.element.style.left = left + 'px';
-        this.element.style.top = top + 'px';
-    }
+// import class
+// food
+import Food from './class/Food';
+// score bar
+import ScoreBar from './class/Bar'
 
 
-}
+
 // test code
 const food = new Food()
-console.log(food.x, food.y);
+setInterval(()=>{
 food.change()
-console.log(food.x, food.y);
+},500)
+
+const bar = new ScoreBar()
+setInterval(() => {
+    bar.addScore()
+}, 500)
